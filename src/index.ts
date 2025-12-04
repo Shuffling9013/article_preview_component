3a8443c477c8f854c399a0e507ab6ab664a9726c
stressfuluser <stressfuluser@proton.me>
2025-12-04 14:54:35 +0800

    Add shared links

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log(42);
+const shareButton = document.querySelector(`button[aria-label="share-button"]`);
+const linksList = document.querySelector(`ul[aria-label="links-menu"]`);
+
+if (null === linksList) {
+  throw Error("Link list not found");
+}
+
+if (null === shareButton) {
+  throw Error("Share button not found");
+}
+
+shareButton.addEventListener("click", (e) => {
+  if (shareButton === e.currentTarget) {
+    shareButton.ariaPressed = "true";
+    linksList.ariaHidden = "false";
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const targets = e.composedPath();
+
+  if (!targets.includes(shareButton) && !targets.includes(linksList)) {
+    shareButton.ariaPressed = "false";
+    linksList.ariaHidden = "true";
+  }
+});
